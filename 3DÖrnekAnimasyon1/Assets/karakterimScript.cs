fb0dcbd5c3cd8a1408c4f234677698d134d6ff30
Servet DEMİR <dmrservet07@gmail.com>
2023-09-12 12:52:10 +0300

    Project İncluded

---
+++ 3DÖrnekAnimasyon1/Assets/karakterimScript.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class karakterimScript : MonoBehaviour
+{
+    Animator animatorum;
+    void Start()
+    {
+        animatorum = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //LAN YURU
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            animatorum.SetBool("yurusun_mu", true);
+        }
+
+        if (Input.GetKeyUp(KeyCode.W))
+        {
+            animatorum.SetBool("yurusun_mu", false);
+        }
+        
+        //LAN KOS
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            animatorum.SetBool("normalkossun_mu", true);
+        }
+
+        if (Input.GetKeyUp(KeyCode.Q))
+        {
+            animatorum.SetBool("normalkossun_mu", false);
+        }
+
+        //LAN HIZLI KOS
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            animatorum.SetBool("hizlikossun_mu", true);
+        }
+
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            animatorum.SetBool("hizlikossun_mu", false);
+        }
+
+
+        //LAN ZIPLA
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            animatorum.SetBool("ziplasin_mi", true);
+        }
+
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            animatorum.SetBool("ziplasin_mi", false);
+        }
+
+    }
+}
